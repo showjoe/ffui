bb776578fda3d50a984d9f310dd8eff33003cdd7
Joseph Barrett <joe@showjoe.com>
2020-02-29 14:09:00 +0000

    removed tabs from list-group.vue

--- src/components/list-group.vue
+++ src/components/list-group.vue
@@ -1,29 +1,29 @@
 <template>
-<ul :class="getClass">
-    <slot ></slot>
-</ul>
+  <ul :class="getClass">
+    <slot></slot>
+  </ul>
 </template>
 <script>
 export default {
-  name:'list-group',
-    props: {
-        flush:Boolean,
-        horizontal:{
-        	default:false
-        }
-    },
-    computed:{
-    	getClass(){
-    		var classes = ['list-group']
-    		if(this.flush) classes.push('list-group-flush')
-    		if(this.horizontal!==false){
-    			if(this.horizontal=='') classes.push('list-group-horizontal')
-    				else {
-    					 classes.push('list-group-horizontal-'+this.horizontal)
-    				}
-    		}
-    		return classes
-    	}
+  name: 'list-group',
+  props: {
+    flush: Boolean,
+    horizontal: {
+      default: false
     }
+  },
+  computed: {
+    getClass() {
+      var classes = ['list-group']
+      if (this.flush) classes.push('list-group-flush')
+      if (this.horizontal !== false) {
+        if (this.horizontal == '') classes.push('list-group-horizontal')
+        else {
+          classes.push('list-group-horizontal-' + this.horizontal)
+        }
+      }
+      return classes
+    }
+  }
 }
 </script>
